905b9b6cd89781f1ff3131113c23c0f7910e7b77
J-Kyu <wjstkdrb91@naver.com>
2021-06-29 10:32:51 +0900

    Linux

--- build-scripts/Step2_CompileScripts.bat
+++ build-scripts/Step2_CompileScripts.bat
@@ -1,2 +1,5 @@
-rem "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" "C:\Source\PROJECT_NAME\PROJECT_NAME.sln" /t:build /p:Platform=Win64;verbosity=diagnostic
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "E:\kyu_shitcreep_Amolang_Dev_9203\VR_Multiplay\VR_Multiplay.sln" /t:build /p:Platform=Win64;verbosity=diagnostic
+rem default: "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" "C:\Source\PROJECT_NAME\PROJECT_NAME.sln" /t:build /p:Platform=Win64;verbosity=diagnostic
+rem Win64: "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "E:\kyu_shitcreep_Amolang_Dev_9203\VR_Multiplay\VR_Multiplay.sln" /t:build /p:Platform=Win64;verbosity=diagnostic
+
+rem Linux
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "E:\kyu_shitcreep_Amolang_Dev_9203\VR_Multiplay\VR_Multiplay.sln" /t:build /p:Platform=Linux;verbosity=diagnostic
--- build-scripts/Step3_BuildFiles.bat
+++ build-scripts/Step3_BuildFiles.bat
@@ -1,2 +1,5 @@
-rem "C:\Program Files\Epic Games\UE_4.17\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="C:\Source\PROJECT_NAME\PROJECT_NAME.uproject" -noP4 -platform=Win64 -clientconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory="C:\Builds\PROJECT_NAME"
-"E:\UE4_26\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="E:\kyu_shitcreep_Amolang_Dev_9203\VR_Multiplay\VR_Multiplay.uproject" -noP4 -platform=Win64 -clientconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory="E:\kyu_shitcreep_Amolang_Dev_9203\VR_Multiplay"
+rem default: "C:\Program Files\Epic Games\UE_4.17\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="C:\Source\PROJECT_NAME\PROJECT_NAME.uproject" -noP4 -platform=Win64 -clientconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory="C:\Builds\PROJECT_NAME"
+rem Win64: "E:\UE4_26\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="E:\kyu_shitcreep_Amolang_Dev_9203\VR_Multiplay\VR_Multiplay.uproject" -noP4 -platform=Win64 -clientconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory="E:\kyu_shitcreep_Amolang_Dev_9203\VR_Multiplay"
+
+rem Linux
+"E:\UE4_26\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="E:\kyu_shitcreep_Amolang_Dev_9203\VR_Multiplay\VR_Multiplay.uproject" -noP4 -build -cook -compressed -stage -noclient -server -serverplatform=Linux -serverconfig=Development -pak -archive -archivedirectory="E:\kyu_shitcreep_Amolang_Dev_9203\VR_Multiplay"
